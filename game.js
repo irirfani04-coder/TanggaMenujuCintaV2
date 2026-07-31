4af32c58d10834167e1793643ca4a9c9f0343d96
irirfani04-coder <ir.irfani04@gmail.com>
2026-08-01 04:34:44 +0700

    Update game.js

--- game.js
+++ game.js
@@ -7,6 +7,12 @@ const board = document.getElementById("board");
 const dice = document.getElementById("dice");
 const roll = document.getElementById("roll");
 const status = document.getElementById("status");
+let posisi = 1;
+
+const player = document.createElement("div");
+player.id = "player";
+player.innerHTML = "❤️";
+
 
 // Membuat 100 kotak
 for(let i=100;i>=1;i--){
@@ -20,13 +26,23 @@ for(let i=100;i>=1;i--){
     board.appendChild(box);
 
 }
-
+document.querySelectorAll(".box")[0].appendChild(player);
 // Tombol Dadu
 
 roll.onclick=function(){
 
     const angka=Math.floor(Math.random()*6)+1;
+posisi += angka;
 
+if (posisi > 100) posisi = 100;
+
+document.querySelectorAll(".box").forEach(box=>{
+    if(box.contains(player)){
+        box.removeChild(player);
+    }
+});
+
+document.querySelectorAll(".box")[posisi-1].appendChild(player);
     dice.innerHTML=angka;
 
     status.innerHTML="💖 Kamu mendapatkan angka "+angka;
