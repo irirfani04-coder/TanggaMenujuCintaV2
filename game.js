b257f7b7039473ba74fb90cc45188584c7df73e9
irirfani04-coder <ir.irfani04@gmail.com>
2026-08-01 09:42:33 +0700

    Update game.js

--- game.js
+++ game.js
@@ -46,5 +46,27 @@ document.querySelectorAll(".box")[100-posisi].appendChild(player);
     dice.innerHTML=angka;
 
     status.innerHTML="💖 Kamu mendapatkan angka "+angka;
-
+cekPertanyaan();
 }; 
+
+const pertanyaan = {
+5:"Apa kesan pertamamu saat mengenalku? ❤️",
+10:"Menurutmu aku orang yang seperti apa?",
+15:"Kalau aku ngajak jalan, kamu mau nggak? 😊",
+20:"Apa yang paling kamu suka dariku?",
+25:"Pernah kangen sama aku nggak? 🤭",
+30:"Menurutmu kita cocok nggak?",
+40:"Kalau aku diam seharian, kamu bakal nyariin nggak?",
+50:"Kasih aku satu pujian yang paling jujur ❤️",
+60:"Kalau aku bilang sayang, kamu jawab apa?",
+70:"Apa impianmu kalau kita bersama?",
+80:"Kalau kita berjodoh, tempat pertama yang ingin kita datangi di mana?",
+90:"Seberapa besar rasa sayangmu kepadaku? ❤️",
+100:"💌 Maukah kamu menjalani kisah ini bersamaku?"
+};
+
+function cekPertanyaan(){
+    if(pertanyaan[posisi]){
+        alert(pertanyaan[posisi]);
+    }
+}
